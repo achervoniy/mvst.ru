dc3bc8b5b4047de57808c6fdd679cc0153ec4acc
DSHelomanov <dshelomanov@Admins-MacBook-Pro.local>
2024-08-01 18:24:16 +0300

    chore: added pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      max_memory_restart: '300M',
+      name: 'must',
+      script: 'node_modules/next/dist/bin/next',
+      env: {
+        NODE_ENV: 'production',
+      },
+      autorestart: true,
+      max_restarts: 3,
+      stop_exit_codes: [0],
+      listen_timeout: 3000,
+      kill_timeout: 5000,
+      restart_delay: 1000,
+      exec_mode: 'cluster',
+      instances: 'max',
+    },
+  ],
+};
