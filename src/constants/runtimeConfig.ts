3f74a19c441413b76c48b8011b20bdcb38e5c333
DSHelomanov <dshelomanov@Admins-MacBook-Pro.local>
2024-08-02 17:00:02 +0300

    chore: update video url

--- src/constants/runtimeConfig.ts
+++ src/constants/runtimeConfig.ts
@@ -3,4 +3,4 @@ export const runtimeConfig = {
   SSR_API_DOMAIN: process.env.SSR_API_DOMAIN,
 };
 
-export const HOME_PAGE_VIDEO_URL = 'https://cdn1.tsum.com/video/vr-video/file1.mp4';
+export const HOME_PAGE_VIDEO_URL = 'https://cdn1.tsum.com/video/mvst-landing-video/low_size_MUST_1920x1080.mp4';
