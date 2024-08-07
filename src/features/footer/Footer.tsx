a2557ce95e0f9331554decacc8fb78406f27b9c2
DSHelomanov <dshelomanov@Admins-MacBook-Pro.local>
2024-08-07 19:29:54 +0300

    chore: remove char ©

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
   return (
     <div className={st.footer}>
       <Typography font="paragraph/regular" align="center" className={st.private}>
-        © Все права защищены 2024
+        Все права защищены 2024
       </Typography>
       <Typography font="paragraph/regular" align="center" decoration="underline">
         Политика конфиденциальности
