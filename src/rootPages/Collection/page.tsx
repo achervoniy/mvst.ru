3c2ad499443218aeb437b81b8d6bca8f0d118387
DSHelomanov <dshelomanov@Admins-MacBook-Pro.local>
2024-10-03 13:11:30 +0300

    chore: remove unused code

--- src/rootPages/Collection/page.tsx
+++ src/rootPages/Collection/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useUnit } from 'effector-react';
-import { useParams, useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 import { LOOK_SLUGS } from '@/constants/runtimeConfig';
@@ -24,9 +24,6 @@ export function CollectionPage() {
   const [activeSlideIndex, setActiveSlideIndex] = useState(1);
   const collectionCounterChanged = useUnit(changeCollectionCounter);
   const version = useUnit(versionField.$value);
-  const nav = useRouter();
-  const p = useSearchParams();
-  const pathname = usePathname();
 
   const lookLen = looks.looks.length;
   const pageTitle = LOOK_SLUGS.women === params.slug ? 'Женская коллекция' : 'Мужская коллекция';
@@ -39,10 +36,8 @@ export function CollectionPage() {
     <section className={st[version]}>
       <div className={st.head}>
         <Typography font="leading/h2" as="h1" align="center">
-          {pageTitle} Page is {p.get('page') ?? ''}
+          {pageTitle}
         </Typography>
-
-        <p onClick={() => nav.push(`${pathname}?page=${Math.random()}`)}>push to page</p>
 
         <Typography font="paragraph/regular" className={st.counter}>
           {activeSlideIndex} / {looks.looks.length}
