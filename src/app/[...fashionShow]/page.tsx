6cbc13119216081006dc0746e0654d175e21c3c5
Дмитрий Шеломанов <dshelomanov@tsum.ru>
2025-12-29 18:44:36 +0300

    refactor: FS pages

---
+++ src/app/[...fashionShow]/page.tsx
@@ -0,0 +1,36 @@
+import { EffectorNext } from '@effector/next';
+import { notFound } from 'next/navigation';
+
+import { FashionShowPage as Page } from '@/rootPages/FashionShow';
+import { MainTemplate } from '@/shared/ui';
+
+import { FS_SCHEME } from '@/constants/fsScheme';
+
+import { Footer } from '@/features/footer';
+import { Header } from '@/features/header';
+
+import { PageProps } from '@/lib/rsc';
+
+export default function DynamicFashionShow({ params }: PageProps<{ fashionShow: string | string[] }>) {
+  const path = Array.isArray(params.fashionShow) ? params.fashionShow.join('/') : params.fashionShow;
+  const FASHION_SHOW_PREFIX = 'fashion-show-';
+
+  if (!path.startsWith(FASHION_SHOW_PREFIX)) {
+    return notFound();
+  }
+
+  const fs = path.slice(FASHION_SHOW_PREFIX.length) as keyof typeof FS_SCHEME;
+  const activeFS = FS_SCHEME[fs];
+
+  if (!fs || fs.includes('/') || !activeFS) {
+    return notFound();
+  }
+
+  return (
+    <EffectorNext>
+      <MainTemplate header={<Header />} footer={<Footer />}>
+        <Page videoSRC={activeFS.video} title={activeFS.title} description={activeFS.description} />
+      </MainTemplate>
+    </EffectorNext>
+  );
+}
