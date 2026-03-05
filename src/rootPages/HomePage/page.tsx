ba3cf8c1996d6525d99f7a886d9d816085c0692e
Дмитрий Шеломанов <dshelomanov@tsum.ru>
2026-03-05 20:44:04 +0300

    replace home page banner

--- src/rootPages/HomePage/page.tsx
+++ src/rootPages/HomePage/page.tsx
@@ -8,10 +8,8 @@ import { AboutMust, CallToBuy, StreamBanner } from '@/features/home';
 
 import { useHash, usePageVisibility } from '@/lib/hooks';
 
-import fw25DesktopAfter from './streamBanners/fs-december-after-stream_desktop.jpg';
-import fw25MobileAfter from './streamBanners/fs-december-after-stream_mobile.jpg';
-import fw25Desktop from './streamBanners/fs-december-before-stream_desktop.jpg';
-import fw25Mobile from './streamBanners/fs-december-before-stream_mobile.jpg';
+import homeBannerDesktop from './homeBannerDesktop.jpg';
+import homeBannerMobile from './homeBannerMobile.jpg';
 
 import st from './styles.module.scss';
 
@@ -20,7 +18,7 @@ export function HomePage() {
   const pageRef = useRef<HTMLDivElement | null>(null);
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const documentVisible = usePageVisibility();
-  const [streamEnded, setStreamEnded] = useState(true);
+  // const [streamEnded, setStreamEnded] = useState(true);
 
   useEffect(() => {
     const target = pageRef.current?.querySelector?.(`[target-id='${hash}']`);
@@ -38,23 +36,25 @@ export function HomePage() {
     }
   }, [documentVisible]);
 
-  useEffect(() => {
-    const now = new Date();
-    const target = new Date(2025, 11, 17, 21, 0, 0);
+  // Проверка стрима
+  // useEffect(() => {
+  //   const now = new Date();
+  //   const target = new Date(2025, 11, 17, 21, 0, 0);
 
-    setStreamEnded(now > target);
-  }, []);
+  //   setStreamEnded(now > target);
+  // }, []);
 
   return (
     <div className={st.page} ref={pageRef}>
       <StreamBanner
-        title="MVST FW25/26"
-        images={
-          streamEnded
-            ? { desktop: fw25DesktopAfter, mobile: fw25MobileAfter }
-            : { desktop: fw25Desktop, mobile: fw25Mobile }
-        }
-        link={streamEnded ? '/collection/must-web' : '/fashion-show-december'}
+        title="MVST SS26"
+        images={{ desktop: homeBannerDesktop, mobile: homeBannerMobile }}
+        // images={
+        //   streamEnded
+        //     ? { desktop: fw25DesktopAfter, mobile: fw25MobileAfter }
+        //     : { desktop: fw25Desktop, mobile: fw25Mobile }
+        // }
+        link={true ? '/collection/must-web' : '/fashion-show-december'}
       />
 
       {/* <div className={st.video}>
